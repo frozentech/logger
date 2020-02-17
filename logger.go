c4bff428d8c9f030dd76e6a5bb77bf1f4763ad1f
Lester Soriano <lestersoriano@reddotpay.com>
2020-02-17 11:32:48 +0800

    Initial Commit

---
+++ logger.go
@@ -0,0 +1,137 @@
+package logger
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Client contains logger interface
+var Client Logger
+
+// Logger contains necessary methods of zap
+type Logger interface {
+	Info(key string, fields ...zapcore.Field)
+	Error(key string, fields ...zapcore.Field)
+	Core() zapcore.Core
+	Sync() error
+	WithOptions(opts ...zap.Option) *zap.Logger
+}
+
+// New initialises a new zap logger
+func New() *zap.Logger {
+	zapLogger, _ := zap.NewProduction()
+	return zapLogger
+}
+
+// MaskCard masks card number and cvv if exists
+func MaskCard(s string) string {
+	m := map[string]interface{}{}
+
+	// Check if string is JSON and mask card
+	if err := json.Unmarshal([]byte(s), &m); err == nil {
+		for k, v := range m {
+			if value, ok := v.(map[string]interface{}); ok {
+				var (
+					b, _ = json.Marshal(value)
+					m2   = map[string]interface{}{}
+				)
+
+				_ = json.Unmarshal([]byte(MaskCard(string(b))), &m2)
+				m[k] = m2
+				continue
+			}
+
+			switch strings.ToLower(k) {
+			case "cvv",
+				"credit_card_cvv",
+				"cardcvc",
+				"securitycode":
+				if value, ok := v.(string); ok {
+					m[k] = mask(value, len(value))
+				}
+			case "number",
+				"cardnumber",
+				"cardnum",
+				"cardno",
+				"accountnumber",
+				"card_number",
+				"card_no":
+				if value, ok := v.(string); ok {
+					m[k] = mask(value, 4)
+				} else if value, ok := v.(float64); ok {
+					m[k] = mask(strconv.Itoa(int(value)), 4)
+				}
+			}
+		}
+
+		b, _ := json.Marshal(m)
+
+		return string(b)
+	}
+
+	// Check if string is URL encoded and does not contain `<`, and mask card
+	if values, err := url.ParseQuery(s); !strings.Contains(s, "<") && strings.Contains(s, "=") && err == nil {
+		newValues := url.Values{}
+		for k, v := range values {
+			switch strings.ToLower(k) {
+			case "cvv", "credit_card_cvv", "cardcvc", "securitycode":
+				newValues[k] = []string{mask(v[0], len(v[0]))}
+			case "number", "cardnumber", "cardnum", "cardno", "accountnumber", "card_no":
+				newValues[k] = []string{mask(v[0], 4)}
+			default:
+				newValues[k] = v
+			}
+		}
+
+		var (
+			buf  strings.Builder
+			keys = make([]string, 0, len(newValues))
+		)
+		for k := range newValues {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		for _, k := range keys {
+			vs := newValues[k]
+			for _, v := range vs {
+				if buf.Len() > 0 {
+					buf.WriteByte('&')
+				}
+				buf.WriteString(k)
+				buf.WriteByte('=')
+				buf.WriteString(v)
+			}
+		}
+
+		return buf.String()
+	}
+
+	// Check if string is XML and mask card
+	r := regexp.MustCompile(`(?i)<(number|cardnumber|cardnum|cardno|accountnumber)>(\d+)<\/(number|cardnumber|cardnum|cardno|accountnumber)>`)
+	if m := r.FindStringSubmatch(s); len(m) == 4 {
+		s = r.ReplaceAllString(s, fmt.Sprintf("<%s>%s</%s>", m[1], mask(m[2], 4), m[3]))
+	}
+
+	r = regexp.MustCompile(`(?i)<(cvv|securitycode|cvNumber)>(\d{3,4})<\/(cvv|securitycode|cvNumber)>`)
+	if m := r.FindStringSubmatch(s); len(m) == 4 {
+		s = r.ReplaceAllString(s, fmt.Sprintf("<%s>%s</%s>", m[1], mask(m[2], len(m[2])), m[3]))
+	}
+
+	return s
+}
+
+func mask(str string, size int) string {
+	if len(str) == size {
+		return strings.Repeat("*", size)
+	}
+
+	return fmt.Sprintf("%s%s", strings.Repeat("*", len(str)-size), str[len(str)-size:])
+}
---
+++ logger_test.go
@@ -0,0 +1,110 @@
+package logger_test
+
+import (
+	"testing"
+
+	"github.com/reddotpay/logger"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+)
+
+func TestLogger_New(t *testing.T) {
+	l := logger.New()
+	assert := assert.New(t)
+	assert.IsType(&zap.Logger{}, l)
+}
+
+func TestLogger_NoMatch(t *testing.T) {
+	s := `helloworld`
+	assert.Equal(t, "helloworld", logger.MaskCard(s))
+}
+
+func TestLogger_JSON_MaskNumber(t *testing.T) {
+	s := `{"number":"4111111111111111"}`
+	assert.Equal(t, "{\"number\":\"************1111\"}", logger.MaskCard(s))
+}
+
+func TestLogger_JSON_MaskSecurityCode(t *testing.T) {
+	s := `{"securityCode":"123"}`
+	assert.Equal(t, "{\"securityCode\":\"***\"}", logger.MaskCard(s))
+}
+
+func TestLogger_JSON_MaskCVV(t *testing.T) {
+	s := `{"cvv":"123"}`
+	assert.Equal(t, "{\"cvv\":\"***\"}", logger.MaskCard(s))
+}
+
+func TestLogger_JSON_MaskCardCvc(t *testing.T) {
+	s := `{"CardCvc":"179"}`
+	assert.Equal(t, "{\"CardCvc\":\"***\"}", logger.MaskCard(s))
+}
+
+func TestLogger_JSON_MaskNestedCard(t *testing.T) {
+	s := `{"card":{"number":"4111111111111111","securityCode":"123"},"payment_method":{"card_number":"4444333322221111","credit_card_cvv":"123"}}`
+	assert.Equal(t, "{\"card\":{\"number\":\"************1111\",\"securityCode\":\"***\"},\"payment_method\":{\"card_number\":\"************1111\",\"credit_card_cvv\":\"***\"}}", logger.MaskCard(s))
+}
+
+func TestLogger_JSON_MaskCard(t *testing.T) {
+	s := `{"number":"4111111111111111","cvv":"123","card":{"number":"4111111111111111"}}`
+	assert.Equal(t, "{\"card\":{\"number\":\"************1111\"},\"cvv\":\"***\",\"number\":\"************1111\"}", logger.MaskCard(s))
+}
+
+func TestLogger_JSON_MaskCard_Nested(t *testing.T) {
+	s := `{"sourceOfFunds":{"provided":{"card":{"number":4111111111111111}}}}`
+	assert.Equal(t, "{\"sourceOfFunds\":{\"provided\":{\"card\":{\"number\":\"************1111\"}}}}", logger.MaskCard(s))
+}
+
+func TestLogger_JSON_MaskCard_EmptyString(t *testing.T) {
+	s := ""
+	assert.Equal(t, "", logger.MaskCard(s))
+}
+
+func TestLogger_JSON_MaskCard_NoMatch(t *testing.T) {
+	s := `{"hello":"world","foo":"bar"}`
+	assert.Equal(t, "{\"foo\":\"bar\",\"hello\":\"world\"}", logger.MaskCard(s))
+}
+
+func TestLogger_URL_MaskNumber(t *testing.T) {
+	s := `number=4111111111111111`
+	assert.Equal(t, "number=************1111", logger.MaskCard(s))
+}
+
+func TestLogger_URL_MaskSecurityCode(t *testing.T) {
+	s := `securityCode=123`
+	assert.Equal(t, "securityCode=***", logger.MaskCard(s))
+}
+
+func TestLogger_URL_MaskCVV(t *testing.T) {
+	s := `cvv=123`
+	assert.Equal(t, "cvv=***", logger.MaskCard(s))
+}
+
+func TestLogger_URL_MaskCard(t *testing.T) {
+	s := `number=4111111111111111&cvv=123`
+	assert.Equal(t, "cvv=***&number=************1111", logger.MaskCard(s))
+}
+
+func TestLogger_URL_MaskCard_EmptyString(t *testing.T) {
+	s := ""
+	assert.Equal(t, "", logger.MaskCard(s))
+}
+
+func TestLogger_URL_MaskCard_NoMatch(t *testing.T) {
+	s := `hello=world&foo=bar`
+	assert.Equal(t, "foo=bar&hello=world", logger.MaskCard(s))
+}
+
+func TestLogger_XML_MaskCard(t *testing.T) {
+	s := `<Message><CardNumber>4111111111111111</CardNumber><Hello>World</Hello><SecurityCode>123</SecurityCode></Message>`
+	assert.Equal(t, "<Message><CardNumber>************1111</CardNumber><Hello>World</Hello><SecurityCode>***</SecurityCode></Message>", logger.MaskCard(s))
+}
+
+func TestLogger_XML_MaskCardNumber(t *testing.T) {
+	s := `<Message><Number>4111111111111111</Number></Message>`
+	assert.Equal(t, "<Message><Number>************1111</Number></Message>", logger.MaskCard(s))
+}
+
+func TestLogger_XML_MaskCVV(t *testing.T) {
+	s := `<Message><cvNumber>123</cvNumber></Message>`
+	assert.Equal(t, "<Message><cvNumber>***</cvNumber></Message>", logger.MaskCard(s))
+}
